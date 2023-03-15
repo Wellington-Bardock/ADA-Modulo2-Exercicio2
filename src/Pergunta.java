85194bb9ad5ba0744afb96946bdcfc90add4b9d5
Wellington <wellington.loliveira@hotmail.com>
2023-03-14 21:00:49 -0300

    Ajuste: 14.03.2023

---
+++ src/Pergunta.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Pergunta {
+
+    public static final String OPCAO_INVALIDA = "Opção Inválida!";
+    public static Scanner sc = new Scanner(System.in);
+
+    public static boolean q (String msg, String complementoMsg) {
+
+        if (msg.equalsIgnoreCase("S")) {
+
+            return true;
+
+        } else if (msg.equalsIgnoreCase("N")) {
+
+            return false;
+
+        } else {
+            Imprimir.i(OPCAO_INVALIDA + "\n" + complementoMsg);
+            msg = sc.nextLine();
+
+            return q (msg, complementoMsg);
+
+        }
+    }
+}
